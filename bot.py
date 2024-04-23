286da708b5dd7064fe771e92fdc385302ca587dd
sp1vak <166430601+sp1vak@users.noreply.github.com>
2024-04-23 16:42:25 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,28 @@
+import os
+import disnake
+
+from disnake.ext import commands
+
+
+bot = commands.Bot(command_prefix=".", help_command=None, intents=disnake.Intents.all(), test_guilds=[1081203721607847956, 1229395978080092191], activity=disnake.Game(name="Вибираю тобі роль..."))
+
+@bot.command()
+@commands.is_owner()
+async def load(ctx, extension):
+    bot.load_extension(f"cogs.{extension}")
+
+@bot.command()
+@commands.is_owner()
+async def unload(ctx, extension):
+    bot.unload_extension(f"cogs.{extension}")
+
+@bot.command()
+@commands.is_owner()
+async def reload(ctx, extension):
+    bot.reload_extension(f"cogs.{extension}")
+
+for filename in os.listdir("cogs"):
+    if filename.endswith(".py"):
+        bot.load_extension(f"cogs.{filename[:-3]}")
+
+bot.run('TOKEN')
